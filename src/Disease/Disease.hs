7989a0a15af08d3aa7116fab75edbabbb713eea0
Sean Gillespie <sean@mistersg.net>
2015-04-06 22:29:39 -0400

    Add random disease generator

--- src/Disease/Disease.hs
+++ src/Disease/Disease.hs
@@ -4,11 +4,23 @@ import System.Random
 
 import Disease.Universe
 
+-- Be careful with the order here, we expect
+-- Alive and Immune to be 1st and 2nd below
 data DiseaseCell = Alive
                  | Immune
                  | Infected
                  | NotUsed -- For cells that "neutral cells"
                  deriving (Show, Enum, Eq)
 
--- TODO[sgillespie]: Generate a random universe
-
+-- | Generate a random disease, using these rules:
+--
+--  1) Exactly 1 infected cell
+--  2) Every other cell is either Alive or Immune (50/50)
+randomDisease :: RandomGen g => Int -> g -> Universe DiseaseCell
+randomDisease size gen = fromList (take size cells) NotUsed
+  where (infectedPos, gen') = randomR (0, size-1) gen
+        (c1, c2)            = splitAt infectedPos (randomCells gen')
+        cells               = c1 ++ [Infected] ++ c2
+        
+randomCells :: RandomGen g => g -> [DiseaseCell]
+randomCells = map toEnum . randomRs (0, 1)
