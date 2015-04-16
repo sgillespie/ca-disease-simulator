499a9fd208d8b51af58330123de0fcb969622a90
Sean Gillespie <sean@mistersg.net>
2015-04-15 22:05:20 -0400

    Add random generators for (Universe2 DiseaseCell)

--- src/Disease/Disease.hs
+++ src/Disease/Disease.hs
@@ -1,6 +1,7 @@
 module Disease.Disease where
 
-import System.Random
+import Control.Comonad
+import System.Random hiding (split)
 
 import Disease.Universe
 
@@ -12,15 +13,46 @@ data DiseaseCell = Alive
                  | NotUsed -- For cells that "neutral cells"
                  deriving (Show, Enum, Eq)
 
--- | Generate a random disease, using these rules:
+-- |Comonad transition rule for a disease cell
+--  Rule:
+--   * If an Alive cell is adjacent to Infected, it becomes Infected
+--   * Otherwise there is no state change
+diseaseRule :: Universe DiseaseCell -> DiseaseCell
+diseaseRule u | extract u == Alive && any (==Infected) (neighbors u) = Infected
+              | otherwise = extract u
+
+diseaseRule2 :: Universe2 DiseaseCell -> DiseaseCell
+diseaseRule2 u | extract u == Alive && any (==Infected) (neighbors2 u) = Infected
+               | otherwise = extract u
+
+-- |Generate a random disease, using these rules:
 --
 --  1) Exactly 1 infected cell
 --  2) Every other cell is either Alive or Immune (50/50)
-randomDisease :: RandomGen g => Int -> g -> Universe DiseaseCell
-randomDisease size gen = fromList (take size cells) NotUsed
+genDisease :: RandomGen g => Int -> g -> Universe DiseaseCell
+genDisease size gen = fromList (take size cells) NotUsed
   where (infectedPos, gen') = randomR (0, size-1) gen
         (c1, c2)            = splitAt infectedPos (randomCells gen')
         cells               = c1 ++ [Infected] ++ c2
         
 randomCells :: RandomGen g => g -> [DiseaseCell]
 randomCells = map toEnum . randomRs (0, 1)
+
+-- |Generate a random disease of Universe2, using the same rules
+--  as randomDisease
+genDisease2 :: RandomGen g => Int -> Int -> g -> Universe2 DiseaseCell
+genDisease2 cols rows gen = fromList2 splitted NotUsed
+  where (infectedPosX, gen')  = randomR (0, cols-1) gen
+        (infectedPosY, gen'') = randomR (0, rows-1) gen'
+        cells = randomCells gen''
+        splitted = split cols rows infectedPosX infectedPosY cells
+        
+
+split :: Int -> Int -> Int -> Int -> [DiseaseCell] -> [[DiseaseCell]]
+split _ 0 _ _ _ = []
+split cols rows infectedPosX 0 cells = row : (split cols (rows-1) infectedPosX (-1) cells'')
+  where (rs, cells')   = splitAt infectedPosX cells
+        (rss, cells'') = splitAt (cols-1-infectedPosX) cells'
+        row = rs ++ [Infected] ++ rss
+split cols rows infectedPosX infectedPosY cells = row : (split cols (rows-1) infectedPosX (infectedPosY-1) cells')
+  where (row, cells') = splitAt cols cells
--- test/QCTests.hs
+++ test/QCTests.hs
@@ -1,6 +1,7 @@
 module QCTests (tests) where
 
 import Control.Comonad
+import System.Random
 
 import Distribution.TestSuite.QuickCheck
 import Test.QuickCheck
@@ -12,7 +13,7 @@ instance (Arbitrary a) => Arbitrary (Universe a) where
   arbitrary = do
     x      <- arbitrary
     size   <- arbitrary
-    offset <- arbitrary
+    offset <- choose (0, size-1)
     ls     <- infiniteListOf arbitrary
     rs     <- infiniteListOf arbitrary
     return $ Universe size offset ls x rs
@@ -24,7 +25,8 @@ instance (Arbitrary a) => Arbitrary (Universe2 a) where
 
 tests :: IO [Test]
 tests = return [testGroup "Universe tests" universeTests,
-                testGroup "Universe2 tests" universe2Tests]
+                testGroup "Universe2 tests" universe2Tests,
+                testGroup "Disease tests" diseaseTests]
   where universeTests = [testProperty "extract = focused element"  prop_extract_id,
                          testProperty "extract . left = head ls"   prop_left_id,
                          testProperty "left u = u"                 prop_left_eq,
@@ -34,11 +36,17 @@ tests = return [testGroup "Universe tests" universeTests,
                          testProperty "extract . duplicate = u"    prop_duplicate_id,
                          testProperty "size = size . duplicate"    prop_duplicate_len,
                          testProperty "neighbors u = [left, extract, right]"
-                                                                   prop_neighbors_id]
+                                                                   prop_neighbors_id,
+                         testProperty "(u =>> extract) == u"       prop_rule_id]
+
         universe2Tests = [testProperty "extract . duplicate = u"   prop_duplicate2_id,
                           testProperty "size = size . duplicate"   prop_duplicate2_len,
                           testProperty "neighbors u = [up, left, extract, right, down]"
-                                                                   prop_neighbors2_id]
+                                                                   prop_neighbors2_id,
+                          testProperty "(u =>> extract) == u"     prop_rule2_id]
+
+        diseaseTests = [testProperty "size genDisease = 1"  prop_gendisease_infected,
+                        testProperty "size genDisease2 = 1" prop_gendisease2_infected]
 
 -- -- --
 -- Universe Tests
@@ -71,6 +79,9 @@ prop_duplicate_len u = size u == (size . duplicate) u
 prop_neighbors_id :: Universe Integer -> Bool
 prop_neighbors_id u = neighbors u == map extract [left u, u, right u]
 
+prop_rule_id :: Universe Integer -> Bool
+prop_rule_id u = (u =>> extract) == u
+
 prop_duplicate2_id :: Universe2 Integer -> Bool
 prop_duplicate2_id u = size (getUniverse2 u) == (size . getUniverse2 . duplicate) u
 
@@ -82,5 +93,37 @@ prop_neighbors2_id u = neighbors2 u == [extract . extract . left . getUniverse2 
                                        (neighbors . extract . getUniverse2) u ++
                                        [extract . extract . right . getUniverse2 $ u]
 
+-- TODO[sgillespie]: =>> only works if all sub-universes have equal size
+prop_rule2_id :: Universe2 Integer -> Property
+prop_rule2_id u = (allEq . toList2 $ u) ==> (u =>> extract) == u
+  where allEq []     = True
+        allEq (x:xs) = all (==x) xs
+
 -- -- --
 -- CA Tests
+newtype GenDisease = GenDisease (Universe DiseaseCell)
+                   deriving (Eq, Show)
+
+instance Arbitrary GenDisease where
+  arbitrary = do
+    seed <- arbitrary
+    size <- suchThat arbitrary (>0)
+    return . GenDisease . genDisease size . mkStdGen $ seed
+
+newtype GenDisease2 = GenDisease2 (Universe2 DiseaseCell)
+                    deriving (Eq, Show)
+
+instance Arbitrary GenDisease2 where
+  arbitrary = do
+    seed <- arbitrary
+    cols <- suchThat arbitrary (>0)
+    rows <- suchThat arbitrary (>0)
+    return . GenDisease2 . genDisease2 cols rows . mkStdGen $ seed
+
+prop_gendisease_infected :: GenDisease -> Bool
+prop_gendisease_infected (GenDisease u) = (length . filter (==Infected) . toList $ u) == 1
+
+prop_gendisease2_infected :: GenDisease2 -> Bool
+prop_gendisease2_infected (GenDisease2 u) = hasOne . filter hasOne . map (filter (==Infected)) $ ls
+  where hasOne = (==1) . length
+        ls = toList2 u
